1dcc8d2463eed63ef7c000611158c5d7e06eac13
Abdelgha-4 <72174073+Abdelgha-4@users.noreply.github.com>
2021-10-16 14:13:28 +0200

    Update churn_data_visualizer.py

--- churn_data_visualizer.py
+++ churn_data_visualizer.py
@@ -8,4 +8,4 @@ dtale_app.JUPYTER_SERVER_PROXY = True
 churn_data = pd.read_csv("BankChurners.csv", index_col='CLIENTNUM', usecols = range(21))
 d = dtale.show(churn_data, allow_cell_edits=False)
 print("Click on the link to explore the data and discover new things!")
-print(f"https://hub.gke2.mybinder.org/user{d._main_url}")
+print(f"https://hub.gke2.mybinder.org{d._main_url}")
